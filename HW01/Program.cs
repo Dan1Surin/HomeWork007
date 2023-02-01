774c563de9d67595e292af52d300808ef464e3d2
Даниил Сурин <dan1surin@gmail.com>
2023-02-01 16:53:01 +0300

    1

---
+++ HW01/Program.cs
@@ -0,0 +1,99 @@
+﻿// Домашняя работа
+
+// Задача 54: Задайте двумерный массив. Напишите программу, которая
+// упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет
+// находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+// (допустимый вариант)
+// 6 16
+// 9 6
+
+// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел.
+//  Напишите программу, которая будет построчно выводить массив, добавляя индексы
+//  каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+int[,] getRandomMatrix(int rows, int columns, int lRange, int rRange)
+{
+    int[,] matrix = new int[rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(lRange, rRange + 1);
+        }
+    }
+    return matrix;
+}
+
+int ROWS = 10;
+int COLUMNS = 10;
+const int LR = 0;
+const int RR = 9;
+
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Matrix = getRandomMatrix(ROWS, COLUMNS, LR, RR);
+PrintMatrix(Matrix);
+
+void ArrangeMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1)-1; j++)
+        {
+            int temp = matr[i,0];
+            if (matr[i, j + 1] > matr[i, j])
+            {
+                temp = matr[i, j];
+                matr[i, j + 1] = matr[i, j];
+                matr[i, j] = temp;
+            }
+            else
+            {
+               
+            }
+        }
+    }
+    Console.WriteLine();
+    PrintMatrix(matr);
+}
+
+ArrangeMatrix(Matrix);
